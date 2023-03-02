88bc747454babdc453d10eacfcaa07f491b9a691
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-02 16:07:56 +0530

    implemented the side drawer and styled it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import NotFound from "./shared/pages/NotFound";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/places/new" element={<NewPlace />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
     </Router>
   );
 }
