523f40afbeac36acfcc44d8269bab74153949390
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-02 16:55:18 +0530

    Created the location navigation functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import NotFound from "./shared/pages/NotFound";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Users />} />
           <Route path="/places/new" element={<NewPlace />} />
+          <Route path="/:userID/places" element={<UserPlaces />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
