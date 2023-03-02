b09a93ef611d1ff073d14dadb4a89d24d0728fd2
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-02 11:49:21 +0530

    create the UseList component

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,9 +1,15 @@
 import React from "react";
+import UserList from "../components/UserList";
 
 export default function Users() {
-  return (
-    <div>
-      <h1>Users works!</h1>
-    </div>
-  );
+  const USERS = [
+    {
+      id: "1234",
+      name: "Robert Smith",
+      image:
+        "https://www.cdc.gov/cancer/prostate/images/man-1200x630.jpg?_=21491",
+      place: 3,
+    },
+  ];
+  return <UserList items={USERS} />;
 }
