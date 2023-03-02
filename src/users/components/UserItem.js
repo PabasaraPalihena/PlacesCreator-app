dc8b856a9fa2faf3cc4ff9d14df5e96d3fca9586
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-02 12:27:11 +0530

    style the user item and the list

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,11 +1,15 @@
 import React from "react";
+
 import "./UserItem.css";
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
+
 export default function UserItem(props) {
   return (
     <li className="user-item">
       <div className="user-item__content">
         <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
+          <Avatar image={props.image} alt={props.name} />
         </div>
         <div className="user-item__info">
           <h2>{props.name}</h2>
