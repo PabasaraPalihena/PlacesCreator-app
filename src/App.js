d2be4bd5c7f5b5c8bab44520efb10fba03ad3c5c
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-02 11:05:05 +0530

    create the routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import NotFound from "./shared/pages/NotFound";
 
 function App() {
-  return <h1>Let's Start</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/places/new" element={<NewPlace />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NewPlace() {
+  return (
+    <div>
+      <h1>New Place works</h1>
+    </div>
+  );
+}
