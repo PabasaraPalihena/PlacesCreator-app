64310ae136e5798c698322742f8129ba89c39e30
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-03 09:26:36 +0530

    Created the model for display the map

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -4,7 +4,7 @@ import UserList from "../components/UserList";
 export default function Users() {
   const USERS = [
     {
-      id: "1234",
+      id: "u1",
       name: "Robert Smith",
       image:
         "https://media.istockphoto.com/id/1200677760/photo/portrait-of-handsome-smiling-young-man-with-crossed-arms.jpg?s=612x612&w=0&k=20&c=g_ZmKDpK9VEEzWw4vJ6O577ENGLTOcrvYeiLxi8mVuo=",
