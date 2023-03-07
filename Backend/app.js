43b96b4ffb925f6cbd4b4b063171daddcf59bd8e
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-07 13:21:40 +0530

    created the place find by ID and useID routes

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const placeRoutes = require("./routes/places");
+
 const app = express();
+
+//Mount routes
+app.use("api/places", placeRoutes);
+
+//Error handling
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
 
 //listen for request
 const PORT = 5005;
---
+++ Backend/routes/places.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const { getPlaceById, getPlaceByUserId } = require("../controllers/places");
+
+router.get("/:pid", getPlaceById);
+router.get("./user/:uid", getPlaceByUserId);
+
+module.exports = router;
