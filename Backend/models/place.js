ce5e151c70c360c065bdbafd2e08824430d8a4bb
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-08 09:52:04 +0530

    created the placeShema model

---
+++ Backend/models/place.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  address: { type: String, required: true },
+  location: {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+  },
+  creator: { typeString, required: true },
+});
+
+const Place = mongoose.model("Place", placeSchema);
+module.exports = Place;
