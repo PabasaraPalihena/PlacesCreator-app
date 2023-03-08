f11ba5007a4b9e05c6a27069ac101dc3388c58df
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-09 04:58:13 +0530

    Modified the app.js in backend with cors

--- Backend/app.js
+++ Backend/app.js
@@ -11,6 +11,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
 //Mount routes
 app.use("/api/places", placeRoutes);
 app.use("/api/users", userRoutes);
