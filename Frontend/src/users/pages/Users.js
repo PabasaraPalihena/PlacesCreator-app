0f8624a7c4d528d1d0e07330c5745e4e3ece1de1
PabasaraPalihena <pabapalihena@gmail.com>
2023-03-09 05:42:44 +0530

    Login request

--- Frontend/src/users/pages/Users.js
+++ Frontend/src/users/pages/Users.js
@@ -1,15 +1,50 @@
-import React from "react";
-import UserList from "../components/UserList";
+import React, { useEffect, useState } from "react";
 
-export default function Users() {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Robert Smith",
-      image:
-        "https://media.istockphoto.com/id/1200677760/photo/portrait-of-handsome-smiling-young-man-with-crossed-arms.jpg?s=612x612&w=0&k=20&c=g_ZmKDpK9VEEzWw4vJ6O577ENGLTOcrvYeiLxi8mVuo=",
-      place: 3,
-    },
-  ];
-  return <UserList items={USERS} />;
-}
+import UsersList from "../components/UserList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+
+const Users = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5005/api/users/");
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
+};
+
+export default Users;
